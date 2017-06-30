081a935cb3374b945cb47d1cbad3b04ccbb4da32
Chris Cieslinski <cieslinskice@ldschurch.org>
2017-06-30 08:33:42 -0600

    Remove unused import

--- src/main/java/org/springframework/data/marklogic/core/mapping/DocumentDescriptor.java
+++ src/main/java/org/springframework/data/marklogic/core/mapping/DocumentDescriptor.java
@@ -1,6 +1,7 @@
 package org.springframework.data.marklogic.core.mapping;
 
 import com.marklogic.client.document.DocumentRecord;
+import com.marklogic.client.impl.JerseyServices;
 import com.marklogic.client.io.DocumentMetadataHandle;
 import com.marklogic.client.io.Format;
 import com.marklogic.client.io.marker.ContentHandle;
