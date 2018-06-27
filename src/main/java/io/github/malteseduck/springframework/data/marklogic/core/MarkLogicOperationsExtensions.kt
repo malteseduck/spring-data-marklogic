6036af6d86b7076e908d836bdb72b082626febc0
CieslinskiCE <cieslinskice@ldschurch.org>
2018-06-27 12:57:44 -0600

    Fix compiler warnings

--- src/main/java/io/github/malteseduck/springframework/data/marklogic/core/MarkLogicOperationsExtensions.kt
+++ src/main/java/io/github/malteseduck/springframework/data/marklogic/core/MarkLogicOperationsExtensions.kt
@@ -5,18 +5,23 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import java.io.InputStream
 
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
 inline fun <reified T : Any> MarkLogicOperations.stream(query: StructuredQueryDefinition, start: Long = 0, length: Int = 10): InputStream =
     stream(query, start, length, T::class.java)
 
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
 inline fun <reified T : Any> MarkLogicOperations.stream(query: StructuredQueryDefinition, pageable: Pageable): InputStream =
     stream(query, pageable, T::class.java)
 
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
 inline fun <reified T : Any> MarkLogicOperations.search(query: StructuredQueryDefinition): List<T> =
     search(query, T::class.java)
 
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
 inline fun <reified T : Any> MarkLogicOperations.search(query: StructuredQueryDefinition, pageable: Pageable): Page<T> =
     search(query, pageable, T::class.java)
 
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
 inline fun <reified T : Any> MarkLogicOperations.search(query: StructuredQueryDefinition, start: Long = 0, length: Int = 10): Page<T> =
     search(query, start, length, T::class.java)
 
@@ -38,9 +43,11 @@ inline fun <reified T : Any> MarkLogicOperations.exists(id: Any): Boolean =
 inline fun <reified T : Any> MarkLogicOperations.read(id: Any): T? =
     read(id, T::class.java)
 
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
 inline fun <reified T : Any> MarkLogicOperations.read(ids: MutableList<Any>): MutableList<T>? =
     read(ids, T::class.java)
 
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
 inline fun <reified T : Any> MarkLogicOperations.count(query: StructuredQueryDefinition): Long =
     count(query, T::class.java)
 
